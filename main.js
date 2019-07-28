98b6696781c50ae225d73c307198509e1f3647b7
Naomi Campos <naomicampos@nkcampos.local>
2019-07-28 17:49:39 -0600

    Make cards persist, style list images, add save and delete from storage methods to List class

--- main.js
+++ main.js
@@ -4,41 +4,114 @@ var makeTaskBtn = document.querySelector('.aside__task--btn');
 var asideListDisplay = document.querySelector('.list__display--area');
 var titleInput = document.querySelector('.aside__title--input');
 var itemInput = document.querySelector('.aside__item--input');
+var clearBtn = document.querySelector('.aside__clear--btn');
+var listDisplay = document.querySelector('.displayed__list');
+// var listSection = document.querySelector('.list__display--area');
 var plusBtn = document.querySelector('.input__plus--img')
+var deleteBtn = document.querySelector('.card__delete--img') 
+var toDoList = [];
+var newTasks = [];
+
+checkLocalStorage();
+appendList();
 
 makeTaskBtn.addEventListener('click', createTaskList);
-plusBtn.addEventListener('click', click)
+plusBtn.addEventListener('click', pushListItems);
+clearBtn.addEventListener('click', clearInputs);
+cardDisplay.addEventListener('click', deleteTaskCard)
 
+function pushListItems() {
+  var newTask = {task: itemInput.value};
+  newTasks.push(newTask);
+  displayList();
+  clearTaskInput();
+}
 
-function saveOnEnter(event) {
-  if (event.key === "Enter") {
-    event.target.blur();
-    console.log('title')
-    addListItems();
+function clearInputs() {
+  itemInput.value = '';
+  titleInput.value = '';
+  listDisplay.innerHTML = '';
+};
+
+function clearTaskInput() {
+  itemInput.value = ''
+};
+
+function getListItems() {
+  var listArray = [];
+  var asideTasks = document.querySelectorAll('.list__items');
+  asideTasks.forEach( function(li) {
+    listArray.push({text: li.innerHTML, checked: false, id: li.id});
+  });
+  return listArray;
+};
+  
+function displayList() {
+    var id = Date.now();
+    listDisplay.insertAdjacentHTML('beforeend', `
+    <li class='list__items' id='${id}'>${itemInput.value}</li>`)
+};
+
+function appendList() {
+  for (var i = 0; i < toDoList.length; i++) {
+    displayTaskList(toDoList[i]);
   }
 }
 
-function addListItems() {
-  asideListDisplay.innerHTML = titleInput.value;
-  asideListDisplay.innerHTML = itemInput.value;
+function createTaskList() {
+  var listArray = getListItems();
+  var list = new List ({title: titleInput.value, listItems: listArray, id: Date.now()})
+  toDoList.push(list);
+  list.saveToStorage(toDoList)
+  displayTaskList(list);
+  clearInputs();
+};
 
+function checkLocalStorage() {
+  if (JSON.parse(localStorage.getItem("toDoItems")) === null) {
+    toDoList = []
+  } else {
+    toDoList = JSON.parse(localStorage.getItem("toDoItems")).map(function(element) {
+      return new List(element)
+    });
+  };
+};
+
+function addTasksToCard(list) {
+  var items = '';
+  list.listItems.forEach( function(eachItem) {
+    items += `<li> <img src='images/checkbox.svg' class='card__unchecked'> ${eachItem.text} </li>`
+  });
+  return items;
+};
+
+function deleteTaskCard(event) {
+  if (event.target.classList[1] === 'card__delete--img') {
+    var list = event.target.closest('.task__card');
+    list.remove();
+  }
 }
 
-function createTaskList() {
+function getListId(event) {
+  var id = parseInt(event.target.closest('.task__card').dataset.id);
+  return id;
+}
+
+function displayTaskList(obj) {
 cardDisplay.insertAdjacentHTML('afterbegin', `<article class='task__card'>
-<section class='task__card--header'>
+<section class='task__card--header' data-id=${obj.id}>
   <p>${obj.title}</p>
 </section>
 <section class='card__body--list'>
-${obj.listItems}
+<ul>
+${addTasksToCard(obj)}
+</ul>
 </section>
 <section class='task__card--footer'>
+  <img src='images/urgent.svg' class='card__footer--img'>
   <p>URGENT</p>
+  <img src='images/delete.svg' class='card__footer--img card__delete--img'>
   <P>DELETE</P>
 </section>
 </article>` )
 };
-
-function click() {
-  console.log("hiiii")
-}
--- todo-list.js
+++ todo-list.js
@@ -1,8 +1,19 @@
 class List {
   constructor(obj) {
     this.title = obj.title;
-    this.listItems = obj.listItems;
-    
+    this.listItems = obj.listItems || [];
+    this.id = obj.id;
+    this.urgent = false;
   }
 
-}
+  saveToStorage(array) {
+    localStorage.setItem("toDoItems", JSON.stringify(array))
+  };
+
+
+  deleteFromStorage(id) {
+		localStorage.getItem("toDoItems", JSON.parse(ideas));
+};
+
+
+};
